93b8da867027e73b5e4ceffe3b2b47ba0c23a6db
rkcoden <khanra4@farmingdale.edu>
2023-02-08 23:59:29 +0000

    done

--- src/main/java/GetMax.java
+++ src/main/java/GetMax.java
@@ -7,6 +7,13 @@ public class GetMax {
      * @return the largest value in arr.
      */
     public int max(int[] arr){
-        return -1;
+       int max = Integer.MIN_VALUE;
+        for(int num : arr){
+            if(num > max){
+                max = num;
+            }
+        }
+
+        return max;
     }
 }
